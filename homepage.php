09a8e54607de3a9ad156a161c9183f2a1b085a02
33432576 <33432576@users.noreply.github.com>
2020-06-11 13:50:57 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,6 +1,10 @@
+<?php
+    /*
+    Template Name: Home Page
+    */
+    ?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <!--        Body section-->
-<?php echo do_shortcode('[sp_wpcarousel id="13"]'); ?>
         <section class="container-fluid landing-links">
         <div class="container">
             <div class="row">
@@ -8,59 +12,61 @@
 <!--                button for text-to-speech function-->
             <p>Listen to this page</p>
                 </div>
-            </div><!--        row-->
+            </div><!--row-->
             <div class="row dementia">
                 <div class="col-sm-1"></div>
-                <div class="col-sm-6 d-bg">
-                    <img class="d-image img-responsive" alt="support group" src="images/support-group-lg.jpg"><img class="d2image img-responsive" alt="support group cropped" src="images/support-group-md.jpg">
+                <div class="col-sm-6 d-bg"> <!--image-->
+                    <img class = 'd-image img-responsive' alt = 'support group' src ='<?php the_field("sectionimage1");?>'>
                 </div>
-                <div class="col-sm-4 d-text">
-                    <h2>What is Dementia?</h2>
-                    <p>Find helpful information about dementia and how it effects people’s lives.</p>
-                    <div class="learnmore" id="whatisdementia">
-                        <a href="#" class="btn red-btn" role="button">
+                <div class="col-sm-4 d-text"> <!--What is Dementia?-->
+                    <h2><?php the_field("titleforsection1"); ?></h2>
+                    <p><?php the_field("sectionparagraph1"); ?> </p>
+                    <div class = 'learnmore' id = 'whatisdementia'>
+                        <a href="<?php the_field('learnmore1'); ?>" class="btn red-btn" role="button">
                             <h4>Learn more</h4>
                         </a>
-                    </div>
+                    </div><!--button what is dem? -->
                 </div>
+                
                 <div class="col-sm-1"></div>
-            </div><!--        what is dementia row-->
+            </div><!--what is dementia row-->
             
             <div class="row cafe">
                 <div class="col-sm-1"></div>
                 
-                <div class="col-sm-4 c-text">
-                    <h2>Memory Cafes</h2>
-                    <p>Learn how Memory Cafès can help people living with dementia and where to find one near you.</p>
-                    <div class="learnmore" id="memorycafes">
-                        <a href="cafes.html" class="btn red-btn" role="button">
+                <div class="col-sm-4 c-text"><!--Memory Cafes-->
+                     <h2><?php the_field("titleforsection2"); ?></h2>
+                    <p><?php the_field("sectionparagraph2"); ?> </p>
+                    <div class="learnmore" id="memorycafes">                       <a href="<?php the_field('learnmore2'); ?>" class="btn red-btn" role="button">
                             <h4>Learn more</h4>
                         </a>
                     </div>
+                    </div>
                 </div>
-                <div class="col-sm-6 c-bg">
-                    <img class="c-image img-responsive" alt="couple at a cafe" src="images/cafe-lg.jpg">
-                    <img class="c2image img-responsive" alt="couple at a cafe cropped" src="images/cafe-md.jpg">
+                
+            <div class="col-sm-6 c-bg"><!--image-->                    
+                <img class = 'c-image img-responsive' alt = 'couple at a cafe' src ='<?php the_field("sectionimage2");?>'>
+                    <!--<img class = 'c2image img-responsive' alt = 'couple at a cafe cropped' src = 'images/cafe-md.jpg'> CUSTOM FIELD FOR RESPONSIVE IMAGE???--> 
                 </div>
-                <div class="col-sm-1"></div>
+
+        <div class="col-sm-1"></div>
             </div><!--        memory cafe row-->
             
              <div class="row organisation">
                  <div class="col-sm-1"></div>
-                <div class="col-sm-6 o-bg">
-                    <img class="o-image img-responsive" alt="supermarket employee" src="images/employee-at-supermarket-lg.jpg"><img class="o2image img-responsive" alt="supermarket employee cropped" src="images/employee-at-supermarket-md.jpg">
+                <div class="col-sm-6 o-bg"><!--image-->
+                <img class = 'o-image img-responsive' alt ='supermarket employee' src ='<?php the_field("sectionimage3");?>'>
                 </div>
-                <div class="col-sm-4 o-text">
-                    <h2>Information for Organisations</h2>
-                    <p>Learn how your business or organisation can make changes to assist those living with dementia.</p>
-                    <div class="learnmore" id="organisations">
-                        <a href="#" class="btn red-btn" role="button">
+                <div class="col-sm-4 o-text"><!--Info for Orgs-->
+                     <h2><?php the_field("titleforsection3"); ?></h2>
+                    <p><?php the_field("sectionparagraph3"); ?> </p>
+                    <div class = 'learnmore' id = 'organisations'>
+                        <a href="<?php the_field('learnmore3'); ?>" class="btn red-btn" role="button">
                             <h4>Learn more</h4>
                         </a>
                     </div>
-                </div>
                  <div class="col-sm-1"></div>
-            </div><!--        info for organisations row-->
-        </div><!--          container-->
+            </div><!--info for organisations row-->
+        </div><!--container-->
     </section>
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
