306938670a14e7bf3f3df5c63a2970af797c450a
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-13 15:02:17 +0800

    comment out breabcrumbs

--- landing_template.php
+++ landing_template.php
@@ -11,15 +11,14 @@
             <div class="row bc-bg">
                 <div class="col-xs-1"></div>
                 <div class="col-xs-10"> 
-                    <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
+<!--                    <div class="breadcrumb"><?php get_breadcrumb(); ?></div>-->
                     
                     <h1><?php the_field("titleforlandingpage"); ?></h1>
                 </div>
                 <div class="col-xs-1"></div>
 
             </div><!--            row with circles background-->
-<!--            Add text-to-speech function HERE
--->
+
             
             <div class="row intro">
                 <div class="col-xs-1"></div>
@@ -32,6 +31,45 @@
                 </div>
                 <div class="col-xs-1"></div>
             </div><!--        row-->
+    
+    <div class="row video-section">
+                <div class="col-xs-1"></div>
+                <div class="col-xs-10">
+                    <p><?php the_field("textforvideointro"); ?> </p>
+                    <p>Setting up a Memory Café is a fairly simple process, but you will need a dedicated group of people who are committed to getting the Memory Café up and running. Once the Memory Café is established, it will almost run itself.
+                        <br>
+                        <br>
+                        The first step is to arrange a meeting between key people in your community. You may include:
+                    </p>
+                    <ul class="a-list">
+                        <li>A café owner</li>
+                        <li>Shopping centre management</li>
+                        <li>A local government representative</li>
+                        <li>A person living with dementia </li>
+                        <li>A carer</li>
+                        <li>And other interested community members</li>
+                    </ul>
+                    <p>In the meeting, talk about how the Memory Café will look, feel and operate.
+                        <br>
+                        <br>
+
+                        Your next step is to identify a suitable café. You may already have a café owner on board, or have an idea of a café you would like to approach.
+                        <br>
+                        <br>
+                        Before deciding on a café, it’s a good idea to consider our dementia enabling environment guidelines, available to download from our website.
+                        <br>
+                        <br>
+                        The guidelines cover things like parking, wheelchair access, noise levels, space and location.
+                        <br>
+                        <br>
+                        One important outcome from the meeting should be the formation of a working group, which is just a group of people who will meet on a regular basis to help get the Memory Café up and running.
+                    </p>
+                    <div class="video" style="height: 480px"></div>
+                </div>
+                <div class="col-xs-1"></div>
+
+            </div><!--            video section-->
+    
            </section>
             
             <section class="container-fluid more-info">
