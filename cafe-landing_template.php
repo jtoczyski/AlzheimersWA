5946be642cedb0e2a8b85520ada7b9540d08a0a7
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-15 10:59:48 +0800

    add iframe map

--- cafe-landing_template.php
+++ cafe-landing_template.php
@@ -69,7 +69,7 @@ bcn_display();
                     </div>
                 </div>
                 
-<!--                INSERT map plug-in HERE-->
+<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
 
             </div>
             <div class="col-xs-1"></div>
