176ad6f52fd794f079f4754f187f629d16dd026e
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-16 16:00:48 +0800

    remove hard code of forms

--- contact.php
+++ contact.php
@@ -41,8 +41,7 @@ endif;
 wp_reset_query();?>
 </div>
                 <div class="col-xs-1"></div>
-
-
+</div>
             <div class="row intro">
                 <?php 
 global $more;//define a global variable
@@ -82,98 +81,7 @@ wp_reset_query();?>
         <section>
 
             <form id="contact-form" method="post" action="contact.php" role="form">
-
-                <div class="messages"></div>
-
-                <div class="controls">
-
-                    <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="form_name">First Name *</label>
-                                <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your first name *" required="required" data-error="First name is required.">
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="form_lastname">Last Name *</label>
-                                <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your last name *" required="required" data-error="Last name is required.">
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-1"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="form_email">Email *</label>
-                                <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="form_phone">Phone *</label>
-                                <input id="form_phone" type="text" name="phone" class="form-control" placeholder="Please enter your phone number *" required="required" data-error="Valid number is required.">
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-1"></div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="form_postcode">Postcode</label>
-                                <input id="form_postcode" type="text" name="email" class="form-control" placeholder="Please enter your postcode" data-error="Valid postcode is required.">
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="form_business">Business</label>
-                                <input id="form_business" type="text" name="business" class="form-control" placeholder="Please enter your company name">
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-1"></div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-10">
-                            <div class="form-group">
-                                <label for="form_message">Message *</label>
-                                <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please, leave us a message."></textarea>
-                                <div class="help-block with-errors"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-1"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-10">
-                            <input type="submit" class="btn btn-message btn-send" value="Send message">
-                        </div>
-                        <div class="col-md-1"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-1"></div>
-                        <div class="col-md-10">
-                            <p class="text-muted">
-                                <strong>*</strong> These fields are required.</p>
-                        </div>
-                        <div class="col-md-1"></div>
-                    </div>
-                </div>
+<!--insert form here in wordpress-->
 
             </form>
         </section><!--        contact form-->
