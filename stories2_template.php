a2576a0114c5c9d11237d0b3d141af6377e91ae4
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-16 22:06:37 +0800

    Create stories2_template.php

---
+++ stories2_template.php
@@ -0,0 +1,123 @@
+<?php
+    /*
+    Template Name: Stories Subpage 2
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<!--        Body section-->
+<div id="page-wrap">
+
+    <section id="container-fluid landing-page">
+        <div class="row triangle-bg">
+            <div class="col-xs-1"></div>
+            <div class="col-xs-10">
+
+                <div class="breadcrumb" typeof="BreadcrumbList" vocab="https://schema.org/">
+                    <?php
+if(function_exists('bcn_display'))
+{
+bcn_display();
+}?>
+                </div>
+
+                <h1><?php the_field("titleforpage"); ?></h1>
+
+                <div class="row">
+                    <!--text-to-speech function-->
+
+                    <button class="text-to-speech" type="button" data-toggle="collpase" data-target="<?php the_field("ttsbutton");?>">
+                        <img class="s-icon" alt="text-to-speech icon" src="<?php the_field("ttsicon");?>">
+
+                        <p><?php the_field("ttsname"); ?> </p>
+                    </button>
+                </div>
+                <div class="row">
+                    <div class="collapse" id="rspeaker">
+
+                        <?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=10');//look for posts that have the category of 10
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 10 get the content
+?>
+                        <div>
+                            <p class=""><?php the_content() ?></p>
+                        </div>
+                        <?php
+endwhile;
+endif;
+wp_reset_query();?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-1"></div>
+        </div><!--            row with triangles background-->
+
+        <div class="row intro">
+            <div class="col-xs-1"></div>
+            <div class="col-xs-10">
+                <!--image-->
+                <img class="landing-img" alt="gathering at memory cafe" src="<?php the_field("storyimage");?>">
+                <p><?php the_field("textbody"); ?> </p>
+
+            </div>
+            <div class="col-xs-1"></div>
+        </div><!--        row-->
+
+    </section>
+
+    <section class="container-fluid more-info">
+        <div class="col-xs-1"></div>
+        <div class="col-xs-10">
+            <h2><?php the_field("titleforothers"); ?></h2>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3><?php the_field("subheading1"); ?></h3>
+                    <p><?php the_field("subexcerpt1"); ?> </p>
+
+                    <div class="learnmore">
+                        <a href="<?php the_field("othersubpagebutton");?>" class="btn red-btn" role="button">
+                            <h4><?php the_field("othersubpagebuttontext1");?></h4>
+                        </a>
+                    </div>
+                    <!--button to resourses for business or local government-->
+
+
+                </div>
+                <div class="col-sm-6">
+                    <h3><?php the_field("subheading2");?></h3>
+                    <p><?php the_field("subexcerpt2");?> </p>
+
+                    <div class="learnmore">
+                        <a href="<?php the_field("othersubpagebutton2");?>" class="btn red-btn" role="button">
+                            <h4><?php the_field("othersubpagebuttontext2");?></h4>
+                        </a>
+                    </div>
+                    <!--button to learnmore success stories-->
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3><?php the_field("subheading3"); ?></h3>
+                    <p><?php the_field("subexcerpt3"); ?> </p>
+
+                    <div class="learnmore">
+                        <a href="<?php the_field("othersubpagebutton3");?>" class="btn red-btn" role="button">
+                            <h4><?php the_field("othersubpagebuttontext3");?></h4>
+                        </a>
+                    </div>
+                    <!--button to resourses for business or local government-->
+
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="col-xs-1"></div>
+    </section>
+
+</div>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
