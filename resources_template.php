25684af135060712c7c8670972287ad2bf9a7aab
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-17 14:45:55 +0800

    finalised breadcrumbs and tts

--- resources_template.php
+++ resources_template.php
@@ -10,47 +10,38 @@
     <section id="container-fluid landing-page">
         <div class="row triangle-bg">
             <div class="col-xs-1"></div>
-            <div class="col-xs-10">
-
-                <div class="breadcrumb" typeof="BreadcrumbList" vocab="https://schema.org/">
-                    <?php
+            <div class="col-xs-10"> 
+                    
+<div class="breadcrumb" typeof="BreadcrumbList" vocab="https://schema.org/">
+<?php
 if(function_exists('bcn_display'))
 {
 bcn_display();
 }?>
-                </div>
+</div>
+                    
+                    <h1><?php the_field("titleforpage"); ?></h1>
+               
+        
+    <div class="row"><!--text-to-speech function-->
+        <div class="text-to-speech">
+<!--button for text-to-speech function-->
 
-                <h1><?php the_field("titleforresourcespage"); ?></h1>
-
-                <div class="row">
-                    <!--text-to-speech function-->
-
-                    <button class="text-to-speech" type="button" data-toggle="collpase" data-target="<?php the_field("ttsbutton");?>">
-                        <img class="s-icon" alt="text-to-speech icon" src="<?php the_field("ttsicon");?>">
-
-                        <p><?php the_field("ttsname"); ?> </p>
-                    </button>
-                </div>
-                <div class="row">
-                    <div class="collapse" id="rspeaker">
-
-                        <?php
+<?php
 global $more;//define a global variable
 $more = 0;// the global varibale is now equal to 0
 query_posts('cat=10');//look for posts that have the category of 10
 if(have_posts()) ://if we have posts to display
 while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 10 get the content
 ?>
-                        <div>
-                            <p class=""><?php the_content() ?></p>
-                        </div>
-                        <?php
+            <div><p class=""><?php the_content() ?></p></div>
+<?php
 endwhile;
 endif;
 wp_reset_query();?>
-                    </div>
-                </div>
-            </div>
+</div>
+    </div>
+                     </div>
             <div class="col-xs-1"></div>
         </div><!--            row with triangles background-->
 
