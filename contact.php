baae2db4d123fc660cfeba7062771d0aecd4874c
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-17 09:31:10 +0800

    make Google map responsive

--- contact.php
+++ contact.php
@@ -105,24 +105,8 @@ wp_reset_query();?>
                     <p><?php the_field("address1"); ?> </p>
                 </div>
                 <div class="col-sm-6">
-                    <div class="mapouter">
-                        <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=55%20Walters%20Drive%20Osborne%20Park%20WA%206017&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2torrentz.net"></a></div>
-                        <style>
-                            .mapouter {
-                                position: relative;
-                                text-align: right;
-                                height: 500px;
-                                width: 600px;
-                            }
-
-                            .gmap_canvas {
-                                overflow: hidden;
-                                background: none !important;
-                                height: 500px;
-                                width: 600px;
-                            }
-
-                        </style>
+                    <div class="google-maps">
+                       <iframe width="600" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=55%20Walters%20Drive%20Osborne%20Park%20WA%206017&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2torrentz.net"></a>
                     </div>
                 </div>
             </div>
