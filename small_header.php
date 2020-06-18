9519b7345af07a9c9e5bff19fcf6449511cdd102
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-06-18 18:19:11 +0800

    create error header

--- small_header.php
+++ small_header.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title><?php
+
+
+    <title>
+        <?php
 		      if (function_exists('is_tag') && is_tag()) {
 		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
 		      elseif (is_archive()) {
@@ -26,13 +23,34 @@
 		   ?>
     </title>
 
+
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet">
-    <?php wp_head(); ?>
+
+
+    <!—Viewport responsive -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+        <!-- This is the traditional favicon.
+		 - size: 16x16 or 32x32
+		 - transparency is OK -->
+        <!—Google fonts -->
+
+            <link href="https://fonts.googleapis.com/css?family=Raleway:400,800" rel="stylesheet">
+
+
+
+            <!-- CSS:  -->
+            <!--remove all our styles and any Javascript code. Wordpress automatically calls javascript /jquery elsewhere, calls to javascript are not needed in the header or the footer now-->
+
+            <?php wp_head(); ?>
+            <!--very important that this is added-->
+
+
 </head>
 
 <body>
+
     <div id="page-wrap">
         <header>
             <!--Navbar-->
@@ -44,7 +62,7 @@
                         <div class="navbar-header">
                             <div class="col-xs-1"></div>
                             <div class="col-xs-4">
-                                <a href="https://dementiafriendlywa.com.au/"><img class="header-img" alt="Alzheimer's WA Logo" src="images/awa-logo-with-tagline-final.png"></a>
+                                <a href="https://dementiafriendlywa.com.au/"><img class="header-img" alt="Alzheimer's WA Logo" src="https://dementiafriendlywa.com.au/wp-content/uploads/2020/06/awa-logo-with-tagline-final.png"></a>
                             </div>
                             <div class="col-xs-1"></div>
                             <div class="col-xs-6 right-menu">
